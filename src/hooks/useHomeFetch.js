88f9ca76ab6a3e91d40c4ef08f1b592a9f676c75
Luxolo Sonto <sontoluxolo17@gmail.com>
2022-02-07 21:33:58 +0200

    Button and Searchbar

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -15,6 +15,7 @@ export const useHomeFetch = () => {
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
 
     const fetchMovies = async (page, searchTerm = "") => {
         try {
@@ -41,8 +42,16 @@ export const useHomeFetch = () => {
         fetchMovies(1, searchTerm);
     }, [searchTerm]);
 
+    // Load
+    useEffect(() => {
+        if(!isLoadingMore) return;
 
-    return {state, loading, error, searchTerm,setSearchTerm};
+        fetchMovies(state.page + 1, searchTerm);
+        setIsLoadingMore(false);
+    }, [isLoadingMore, searchTerm, state.page]);
+
+
+    return {state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore};
 };
 
 
