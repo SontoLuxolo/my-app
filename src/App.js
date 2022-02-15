0a209ef7ecf9c27c92f48f06980f8617eaf29bde
Luxolo Sonto <sontoluxolo17@gmail.com>
2022-02-15 20:37:14 +0200

    fetchmovie routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Components
 import Header from './components/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
 // Styles
 import { GlobalStyle } from './GlobalStyle';
@@ -12,9 +14,10 @@ const App = () => (
     <Router>
       <Header />
         <Routes>
-          
+          <Route path='/' element={<Home />} />
+          <Route path='/:movieId' element={<Movie />} />
+          <Route path='/*' element={<NotFound />} />
         </Routes>
-        <Home />
       <GlobalStyle />
     </Router>
   );
